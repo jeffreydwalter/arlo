90dd3a16fca93a44c57c0bf3f68758782a079894
Jeff Walter <jeff.walter@quest.com>
2018-01-30 10:55:47 -0600

    Fixed streaming video example.

---
+++ examples/arlo-fullscreensnapshot.py
@@ -0,0 +1,31 @@
+from Arlo import Arlo
+
+USERNAME = 'user@example.com'
+PASSWORD = 'supersecretpassword'
+
+try:
+	# Instantiating the Arlo object automatically calls Login(), which returns an oAuth token that gets cached.
+	# Subsequent successful calls to login will update the oAuth token.
+	arlo = Arlo(USERNAME, PASSWORD)
+	# At this point you're logged into Arlo.
+
+	# Get the list of devices and filter on device type to only get the basestation.
+	# This will return an array which includes all of the basestation's associated metadata.
+	basestations = arlo.GetDevices('basestation')
+
+        # Get the list of devices and filter on device type to only get the camera.
+        # This will return an array which includes all of the camera's associated metadata.
+        cameras = arlo.GetDevices('camera')
+
+        # Tells the Arlo basestation to trigger a snapshot on the given camera.
+        # This snapshot is not instantaneous, so this method waits for the response and returns the url
+        # for the snapshot, which is stored on the Amazon AWS servers.
+        snapshot_url = arlo.TriggerFullFrameSnapshot(basestation, camera)
+
+        # This method requests the snapshot for the given url and writes the image data to the location specified.
+        # In this case, to the current directory as a file named "snapshot.jpg"
+        # Note: Snapshots are in .jpg format.
+        arlo.DownloadSnapshot(snapshot_url, 'snapshot.jpg')
+
+except Exception as e:
+    print(e)
---
+++ examples/arlo-streamingvideo.py
@@ -0,0 +1,42 @@
+from Arlo import Arlo
+
+from subprocess import call
+
+USERNAME = 'user@example.com'
+PASSWORD = 'supersecretpassword'
+
+try:
+
+    # Instantiating the Arlo object automatically calls Login(),
+    # which returns an oAuth token that gets cached.
+    # Subsequent successful calls to login will update the oAuth token.
+    arlo = Arlo(USERNAME, PASSWORD)
+    # At this point you're logged into Arlo.
+
+    # Get the list of devices and filter on device type to only get the cameras.
+    # This will return an array which includes all of the canera's associated metadata.
+    cameras = arlo.GetDevices('camera')
+
+    # Get the list of devices and filter on device type to only get the basestation.
+    # This will return an array which includes all of the basestation's associated metadata.
+    basestations = arlo.GetDevices('basestation')
+
+    # Open the event stream to act as a keep-alive for our stream.
+    arlo.Subscribe(basestations[0])
+
+    # Send the command to start the stream and return the stream url.
+    url = arlo.StartStream(cameras[0])
+
+    # Record the stream to a file named 'test.mp4'.
+    # **Requires ffmpeg 3.4 or greater.**
+    # For this example, I'm going to open ffmpeg.
+    # Crucially important is the '-t' flag, which specifies a recording time. (See the ffmpeg documentation.)
+    # This is just a crude example, but hopefully it will give you some ideas.
+    # You can use any number of libraries to do the actual streaming. OpenCV or VLC are both good choices.
+    # NOTE: This will print the output of ffmpeg to STDOUT/STDERR. If you don't want that, you will
+    # need to pass additional arguments to handle those streams.
+
+    call(['ffmpeg', '-re', '-i', url, '-t', '10', '-acodec', 'copy', '-vcodec', 'copy', 'test.mp4'])
+
+except Exception as e:
+    print(e)
