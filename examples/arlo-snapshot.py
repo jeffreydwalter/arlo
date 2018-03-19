a36389782b4933d9c83d395885f915aac0dd59fa
jeffreydwalter <jeffreydwalter@gmail.com>
2018-03-18 22:17:28 -0500

    Update arlo-snapshot.py

--- examples/arlo-snapshot.py
+++ examples/arlo-snapshot.py
@@ -10,15 +10,15 @@ try:
     # At this point you're logged into Arlo.
 
     # Get the list of devices and filter on device type to only get the basestation.
-	# This will return an array which includes all of the basestation's associated metadata.
-	basestations = arlo.GetDevices('basestation')
+    # This will return an array which includes all of the basestation's associated metadata.
+    basestations = arlo.GetDevices('basestation')
     
     # Get the list of devices and filter on device type to only get the cameras.
     # This will return an array of cameras, including all of the cameras' associated metadata.
     cameras = arlo.GetDevices('camera')
 
     # Starting recording with a camera.
-    arlo.StartRecording(cameras[0]);
+    arlo.StartRecording(basestations[0], cameras[0]);
 
     # Wait for 4 seconds while the camera records. (There are probably better ways to do this, but you get the idea.)
     time.sleep(4)
