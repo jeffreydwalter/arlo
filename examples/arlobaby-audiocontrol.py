bfacc7d5572841fffa1bdc6056fd3ae7b7710c5e
Jurgen Weber <j.weber@foodpanda.com>
2018-04-25 09:45:01 +1000

    setup Baby Arlo Audio control

---
+++ examples/arlobaby-audiocontrol.py
@@ -0,0 +1,39 @@
+from Arlo import Arlo
+
+USERNAME = 'user@example.com'
+PASSWORD = 'supersecretpassword'
+
+try:
+	# Instantiating the Arlo object automatically calls Login(), which returns an oAuth token that gets cached.
+	# Subsequent successful calls to login will update the oAuth token.
+	arlo = Arlo(USERNAME, PASSWORD)
+	# At this point you're logged into Arlo.
+
+	# Get the list of devices and filter on device type to only get the basestation.
+	# This will return an array which includes all of the basestation's associated metadata.
+	basestations = arlo.GetDevices('basestation')
+
+        # Get the list of devices and filter on device type to only get the camera.
+        # This will return an array which includes all of the camera's associated metadata.
+        cameras = arlo.GetDevices('camera')
+
+        # Get current state payload
+        arlo.GetAudioPlayback(cameras[0])
+
+        # Start playing
+        arlo.PlayTrack(cameras[0], track_id, position)
+
+        # Pause the track
+        arlo.PauseTrack(cameras[0])
+
+        # Skip the track
+        arlo.SkipTrack(cameras[0])
+
+        # record temperature history
+        arlo.SetSleepTimerOn(cameras[0])
+
+        # Set the temperature unit to Celcius
+        arlo.SetLoopBackModeContinuous(cameras[0])
+
+except Exception as e:
+    print(e)
