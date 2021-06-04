a40a6abad54f0d8b550b9e28d78702f2f679f610
jwalter1-quest <jeff.walter@quest.com>
2021-06-04 10:28:35 -0500

    Add schemaVersion header back.

--- arlo.py
+++ arlo.py
@@ -138,6 +138,7 @@ class Arlo(object):
         
         headers = {
             'DNT': '1',
+            'schemaVersion': '1',
             'Auth-Version': '2',
             'Content-Type': 'application/json; charset=UTF-8',
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Mobile/15B202 NETGEAR/v1 (iOS Vuezone)',
